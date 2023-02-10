ff979093272cd5664e88395e7a30cb8af8ff5aba
furkanGit <furkancaliskan9801@gmail.com>
2023-02-10 10:34:08 +0100

    upgrade factory

--- database/factories/ProjectFactory.php
+++ database/factories/ProjectFactory.php
@@ -21,7 +21,7 @@ class ProjectFactory extends Factory
             'description' => fake()->boolean()
             ? fake()->paragraph()
             : '',
-            'main_image' => fake()->imageUrl(),
+            'main_image' => fake()->imageUrl(640, 480, 'animals', true),
             'release_date' => fake()->date(),
             'repo_link' => fake()->url(),
 
