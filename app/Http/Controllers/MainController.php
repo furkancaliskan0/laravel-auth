dedec95d500c9fdf2cd785a3233319fdee621064
furkanGit <furkancaliskan9801@gmail.com>
2023-02-10 13:10:27 +0100

    Added create and function  and store, edit and delete route

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -10,13 +10,75 @@ class MainController extends Controller
 {
     public function home()
     {
-        $projects = Project::all();
+        $projects = Project::orderBy('created_at', 'DESC')->get();
 
         return view('pages.home', compact('projects'));
     }
 
     public function logged()
     {
-        return view('pages.logged');
+        $projects = Project::orderBy('created_at', 'DESC')->get();
+
+        return view('pages.logged', compact('projects'));
+    }
+
+    //SHOW:
+    public function projectShow(Project $project)
+    {
+        return view('pages.project.show', compact('project'));
+    }
+
+    // Create Logged Private Single Content
+    public function projectCreate()
+    {
+
+        return view('pages.project.create');
+
+    }
+
+    public function store(Request $request)
+    {
+
+        $data = $request->validate([
+            'name' => 'required|string|min:3|max:64|unique:projects,name',
+            'description' => 'nullable|string',
+            'main_image' => 'required|string|unique:projects,main_image',
+            'release_date' => 'required|date|before:today',
+            'repo_link' => 'required|string|unique:projects,repo_link',
+        ]);
+
+        $project = Project::create($data);
+
+        return redirect()->route('project.show', $project);
+    }
+
+    public function edit(Project $project)
+    {
+
+        return view('pages.project.edit', compact('project'));
+    }
+    public function update(Request $request, Project $project)
+    {
+
+        $data = $request->validate([
+            'name' => 'required|string|min:3|max:64|unique:projects,name,' . $project->id,
+            'description' => 'nullable|string',
+            'main_image' => 'required|string|unique:projects,main_image,' . $project->id,
+            'release_date' => 'required|date|before:today',
+            'repo_link' => 'required|string|unique:projects,repo_link,' . $project->id,
+        ]);
+
+        $project->update($data);
+        $project->save();
+
+        return redirect()->route('project.show', $project);
+    }
+
+    public function delete(Project $project)
+    {
+
+        $project->delete();
+
+        return redirect()->route('home');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -5,15 +5,50 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MainController;
 
 
+// ROUTE HOME :
 Route::get('/', [MainController::class, 'home'])
     ->name('home');
 
+
+// ROUTE LOGGED :
 Route::middleware(['auth', 'verified'])
     ->name('logged.')
     ->prefix('logged')
     ->group(function () {
         Route::get('/', [MainController::class, 'logged'])
             ->name('logged');
+    });
+
+// ROUTE SHOW :
+Route::get('/project/show/{project}', [MainController::class, 'projectShow'])
+    ->name('project.show');
+
+Route::middleware(['auth', 'verified'])
+    ->name('admin.')
+    ->prefix('admin')
+    ->group(function () {
+
+        // ROUTE CREATE :
+    
+        Route::get('/project/create', [MainController::class, 'create'])
+            ->name('project.create');
+        // ROUTE STORE :
+        Route::post('/project/create', [MainController::class, 'store'])
+            ->name('project.store');
+        // ROUTE EDIT :
+    
+        Route::get('/project/edit/{project}', [MainController::class, 'edit'])
+            ->name('project.edit');
+
+        // ROUTE UPDATE :
+        Route::post('/project/edit/{project}', [MainController::class, 'update'])
+            ->name('project.update');
+
+        // ROUTE DELETE :
+    
+        Route::get('/project/delete/{project}', [MainController::class, 'delete'])
+            ->name('project.delete');
+
     });
 
 Route::get('/dashboard', function () {
